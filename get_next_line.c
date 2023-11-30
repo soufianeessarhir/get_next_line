2d61515d875a3485cfc8a25ec2db5e2f168440ff
Soufiane Essarhir <soufianeessarhir@gmail.com>
2023-11-30 22:44:14 +0100

    BB

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 05:30:11 by sessarhi          #+#    #+#             */
-/*   Updated: 2023/11/30 06:29:18 by sessarhi         ###   ########.fr       */
+/*   Updated: 2023/11/30 22:43:17 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ char *get_next_line(int fd)
 int main ()
 {
 	int fd = open("text.txt",O_RDONLY);
-
 		printf("%s\n", get_next_line(fd));
 		printf("%s\n", get_next_line(fd));
 		printf("%s\n", get_next_line(fd));
@@ -59,5 +58,9 @@ int main ()
 		printf("%s\n", get_next_line(fd));	
 		printf("%s\n", get_next_line(fd));	
 		printf("%s\n", get_next_line(fd));	
+		// printf("%s\n", get_next_line(fd));	
+ 		// system("leaks a.out");
+ 		// close(1);
+	
 
 }
