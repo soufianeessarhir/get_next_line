05f5043d02d25cacc901f350bed5377554722bb9
Soufiane Essarhir <soufianeessarhir@gmail.com>
2023-12-02 13:21:57 +0100

    nice

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,64 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 05:30:11 by sessarhi          #+#    #+#             */
-/*   Updated: 2023/12/01 11:47:01 by sessarhi         ###   ########.fr       */
+/*   Updated: 2023/12/02 13:18:48 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
+char	*haha(char *s, char **str,int *flag)
+{
+    s = ft_substr(*str, 0, ft_strlen(*str));
+	*flag = 0;
+    free(*str);
+    return s;
+}
+char *ft_backup(char *str,char *buff,int newline)
+{
+	char *backup;
+	free(buff);
+	backup = ft_substr(str,newline,ft_strlen(str) - newline);
+	free(str);
+	return backup;
+}
 char *get_next_line(int fd)
 {
-	static char *str;
-	// int line;
-	char *s;
-	char *buff;
+    char *buff;
+    char *s;
+    static char *str;
+	int newline;
+	static int flag;
 
-	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	buff[BUFFER_SIZE + 1] = '\0';
-	while (read(fd,buff,BUFFER_SIZE)>= 0)
-	{
-		ft_strjoin(str,buff);
-		line = ft_strchr(str,'\n');
-		if (line != -1)
-		{
-			
-		}
-		
-	}
-	
+	flag = 1;
+    buff = malloc(BUFFER_SIZE + 1);
+    if (!buff)
+        return NULL;
+    while (flag && read(fd, buff, BUFFER_SIZE) >= 0)
+    {
+        buff[BUFFER_SIZE] = '\0';
+        str = ft_strjoin(str, buff);
+        newline = ft_strchr(str, '\n');
+        if (newline != -1 && flag)
+        {
+            s = ft_substr(str, 0, newline);
+            str = ft_backup(str,buff,newline);
+            return s;
+        }
+		else if (str && flag && newline == -1)
+			return(haha(s,&str,&flag));
+    }
+    return NULL;
 }
 int main ()
 {
 	int fd = open("text.txt",O_RDONLY);
-	printf("%s", get_next_line(fd));
-	
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));
+		printf("%s", get_next_line(fd));	
 }
